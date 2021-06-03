c6a6c02fc1717bfc6b7296b7c4b9fb25eeb47135
“Beck <beckster1999@gmail.com>
2021-06-03 22:04:09 +0000

    added account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -25,8 +29,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "password"
       }}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not is_logged_in?
+    #Try to login before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    #invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    #Valid activation token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    #valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash[:error]
+    assert is_logged_in?
   end
 end
